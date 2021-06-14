4ba031a87e81f953fae412d78f8224420ed35122
Ishita Chawla <84702817+ishitachawla@users.noreply.github.com>
2021-06-14 16:04:15 +0000

    -

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ fs.readdir('./', (err, files) => {
     var secret_token = core.getInput('GITHUB_TOKEN');
 //    const regex = /^(.*)github.com:(.*)\/(.*)\.git$/; 
     //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
-    const octokit = new Octokit({ auth: `personal-access-token123` });
+    const octokit = new Octokit({ auth: secret_token });
     if(  secret_token === "")
     {console.log("blank value");}
       else
