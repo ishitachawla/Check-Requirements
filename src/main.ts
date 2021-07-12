26a8f2512bddf9c58a98af3578cf73af8134043c
Ishita Chawla <84702817+ishitachawla@users.noreply.github.com>
2021-07-12 14:49:19 +0000

    -

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { Octokit } from '@octokit/core';
 async function main() { 
   const repos= core.getInput('repo_name');
 
-  fs.readdir('github.com/ishitachawla/Requirement-testing/', (err, files) => {
+  fs.readdir('https://github.com/ishitachawla/Requirement-testing.git', (err, files) => {
     if (err){
       console.log(err);
     }
